8d3dff55b10417909a00adf3ac78cc043d895042
JarlBurget <jarl-ivor.burget@voco.ee>
2025-09-10 09:54:10 +0300

    Java basic 4

---
+++ Exercise4.java
@@ -0,0 +1,19 @@
+public class Exercise4 {
+    public static void main(String[] args) {
+        int chickens = 9000;
+        double baconKg = 0.1;
+        String tractor = "Zetor";
+
+        System.out.println("Chickens:");
+        System.out.println(chickens);
+        System.out.println("Bacon (kg):");
+        System.out.println(baconKg);
+        System.out.println("A tractor:");
+        System.out.println(tractor);
+
+        System.out.println("\nIn a nutshell:");
+        System.out.println(chickens);
+        System.out.println(baconKg);
+        System.out.println(tractor);
+    }
+}
