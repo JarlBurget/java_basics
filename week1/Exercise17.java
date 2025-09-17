c44b70a6b65aa9850d11c4b299cae37f0c0bdc83
JarlBurget <jarl-ivor.burget@voco.ee>
2025-09-17 08:40:26 +0300

    week 1, execise 17

---
+++ week1/Exercise17.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Exercise17 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Kasutan jälle doubleit et saaks kasutada ka komadega arve. Sama tulemuse saab kui kasutada int
+        System.out.print("Type the first number: ");
+        double firstNumber = scanner.nextDouble();
+
+        System.out.print("Type the second number: ");
+        double secondNumber = scanner.nextDouble();
+
+        if (firstNumber > secondNumber) {
+            System.out.println("Greater number: " + firstNumber);
+        } else if (secondNumber > firstNumber) {
+            System.out.println("Greater number: " + secondNumber);
+        } else {
+            System.out.println("The numbers are equal.");
+        }
+
+        scanner.close();
+    }
+}
